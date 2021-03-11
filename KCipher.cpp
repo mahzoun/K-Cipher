5aedb5a17d2ac400c036e1e735bfe14794fc5f3b
mahzoun <mohammad.mahzoun@gmail.com>
2021-03-11 16:10:45 +0100

    implement enc function

--- KCipher.cpp
+++ KCipher.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 
 template<size_t size>
-bitset<size> &operator+(bitset<size> &A, bitset<size> &B) noexcept {
+bitset<size> operator+(bitset<size> &A, bitset<size> &B) noexcept {
     bitset<size> SUM;
     bool carry = 0;
     for (int i = 0; i < size; i++) {
@@ -24,7 +24,7 @@ bitset<size> &operator+(bitset<size> &A, bitset<size> &B) noexcept {
 bitset<N> KCipher::BitReordering(bitset<128> input, int index) {
     bitset<N> output;
     for (int i = 0; i < N; i++) {
-        int new_index = __kcipher_128_bitreordering[index][i];
+        int new_index = reordering[index][i];
         output[new_index] = input[i];
     }
     return output;
@@ -33,27 +33,30 @@ bitset<N> KCipher::BitReordering(bitset<128> input, int index) {
 bitset<N> KCipher::SBox(bitset<N> input, bitset<N> rand[], int index) {
     bitset<N> output;
     for (int block = 0; block < N; block += M) {
-        bitset<8> cur_block, cur_r0, cur_r1;
+        bitset<M> cur_block, cur_r0, cur_r1;
         long block_val, r0_val, r1_val;
         for (int i = block; i < block + M; i++) {
-            cur_block[i] = input[i];
-            cur_r0[i] = rand[2 * index][i];
-            cur_r1[i] = rand[2 * index + 1][i];
+            cur_block[i - block] = input[i];
+            cur_r0[i - block] = rand[2 * index][i];
+            cur_r1[i - block] = rand[2 * index + 1][i];
         }
         block_val = cur_block.to_ulong();
         r0_val = cur_r0.to_ulong();
         r1_val = cur_r1.to_ulong();
         long t = sbox[block_val ^ r0_val] + r1_val;
         t = t % (1 << M);
-        t = t << 2 | t >> M - 2; //this is weird
+        //t = t << 2 | t >> M - 2; //this is weird
         cur_block = t;
         for (int i = block; i < block + M; i++) {
-            output[i] = cur_block[i];
+            output[i] = cur_block[i - block];
         }
     }
 }
 
-bitset<128> KCipher::EncCPA(bitset<128> input, bitset<128> K[4], bitset<128> rand[]) {
+bitset<128> KCipher::EncCPA(bitset<128> input, bitset<128> key, bitset<128> rand[]) {
+    cerr << input << endl;
+    bitset<N> K[3];
+    KeyExpansion(key, K);
     for (int i = 0; i < 3; i++) {
         input = input + K[i];
         input = BitReordering(input, i);
@@ -61,4 +64,26 @@ bitset<128> KCipher::EncCPA(bitset<128> input, bitset<128> K[4], bitset<128> ran
     }
     bitset<N> veil = BitReordering(K[2], 3);
     return input ^ veil;
+}
+
+void KCipher::KeyExpansion(bitset<N> key, bitset<N> K[]) {
+    bitset<N> C, U, rand[4];
+    bitset<64> t[2];
+    t[0] = __kcipher_range_65_128_const_1[0];
+    t[1] = __kcipher_range_65_128_const_1[1];
+    for (int j = 0; j < 128; j++)
+        C[j] = j < 64 ? t[0][j] : t[1][j - 64];
+    K[0] = key;
+    U = C + key;
+    U = BitReordering(U, 4);
+    U = SBox(U, rand, -1);
+    K[1] = BitReordering(U, 5);
+    t[0] = __kcipher_range_65_128_const_2[0];
+    t[1] = __kcipher_range_65_128_const_2[1];
+    for (int j = 0; j < 128; j++)
+        C[j] = j < 64 ? t[0][j] : t[1][j - 64];
+    U = C + K[1];
+    U = BitReordering(U, 6);
+    U = SBox(U, rand, -1);
+    K[2] = BitReordering(U, 7);
 }
--- main.cpp
+++ main.cpp
@@ -5,13 +5,15 @@
 using namespace std;
 
 int main(int argc, char **argv) {
+    KCipher kcipher;
     uint64_t plaintext_val[2] = {0x2b58bffc83cc6c39, 0x24f22580b2107da7};
     uint64_t key_val[14] = {0x27aef6116c4db0e6, 0x2779d02d3094d1df, 0xb8c0ad914767ba80, 0x6ca98308d45d1f79,
                             0xd75f78588ceaf21a, 0x3190bc4bfa457450, 0x92fd07e27f65d6c2, 0xd632a79fd631870c,
                             0x235548ef50bd1c1f, 0x002440be99b4d4ba, 0x1d038d1d35d9cd0f, 0xb1336f128aaebf73,
                             0x8028a087933b6f4a, 0x74fd2d5530ebb1f5};
+    uint64_t cipher_val[2] = {0xa3f53f715d01e6eb, 0xa8c1904ee7567837};
     bitset<64> t[2];
-    bitset<N> input, key, rand[6];
+    bitset<N> input, key, rand[6], output;
     t[0] = plaintext_val[0];
     t[1] = plaintext_val[1];
     for (int i = 0; i < 128; i++)
@@ -26,9 +28,13 @@ int main(int argc, char **argv) {
         for (int j = 0; j < 128; j++)
             rand[i][j] = j < 64 ? t[0][j] : t[1][j - 64];
     }
-    cout << input << endl << key << endl;
-    for (int i = 0; i < 6; i++)
-        cout << rand[i] << endl;
+//    cout << input << endl << key << endl;
+//    for (int i = 0; i < 6; i++)
+//        cout << rand[i] << endl;
+
+    output = kcipher.EncCPA(input, key, rand);
+
+    cout << output << endl;
     return 0;
 }
 
