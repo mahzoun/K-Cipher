98b44a8d345b162e955b89f3e6cfac20f5da679d
mahzoun <mohammad.mahzoun@gmail.com>
2021-03-11 12:06:07 +0100

    add enc function

--- KCipher.cpp
+++ KCipher.cpp
@@ -10,11 +10,11 @@
 
 using namespace std;
 
-template <size_t size>
-bitset<size>& operator+(bitset<size>& A, bitset<size>& B) noexcept {
+template<size_t size>
+bitset<size> &operator+(bitset<size> &A, bitset<size> &B) noexcept {
     bitset<size> SUM;
     bool carry = 0;
-    for(int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
         SUM[i] = A[i] ^ B[i] ^ carry;
         carry = (A[i] & B[i]) | (A[i] & carry) | (B[i] & carry);
     }
@@ -23,19 +23,38 @@ bitset<size>& operator+(bitset<size>& A, bitset<size>& B) noexcept {
 
 bitset<N> KCipher::BitReordering(bitset<128> input, int index) {
     bitset<N> output;
-    for(int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) {
         int new_index = __kcipher_128_bitreordering[index][i];
         output[new_index] = input[i];
     }
     return output;
 }
 
-bitset<128> KCipher::SBox(bitset<128> input, bitset<128> rand, int index) {
-    return input;
+bitset<N> KCipher::SBox(bitset<N> input, bitset<N> rand[], int index) {
+    bitset<N> output;
+    for (int block = 0; block < N; block += M) {
+        bitset<8> cur_block, cur_r0, cur_r1;
+        long block_val, r0_val, r1_val;
+        for (int i = block; i < block + M; i++) {
+            cur_block[i] = input[i];
+            cur_r0[i] = rand[2 * index][i];
+            cur_r1[i] = rand[2 * index + 1][i];
+        }
+        block_val = cur_block.to_ulong();
+        r0_val = cur_r0.to_ulong();
+        r1_val = cur_r1.to_ulong();
+        long t = sbox[block_val ^ r0_val] + r1_val;
+        t = t % (1 << M);
+        t = t << 2 | t >> M - 2; //this is weird
+        cur_block = t;
+        for (int i = block; i < block + M; i++) {
+            output[i] = cur_block[i];
+        }
+    }
 }
 
-bitset<128> KCipher::EncCPA(bitset<128>input , bitset<128> K[4], bitset<128> rand) {
-    for(int i = 0; i < 3; i++) {
+bitset<128> KCipher::EncCPA(bitset<128> input, bitset<128> K[4], bitset<128> rand[]) {
+    for (int i = 0; i < 3; i++) {
         input = input + K[i];
         input = BitReordering(input, i);
         input = SBox(input, rand, i);
--- main.cpp
+++ main.cpp
@@ -1,70 +1,34 @@
 #include "KCipher.h"
 #include <iostream>
 #include <cmath>
-#include <givaro/gfq.h>
-#include <givaro/givpower.h>
-#include <givaro/givtimer.h>
 
 using namespace std;
-using namespace Givaro;
 
-int main(int argc, char** argv)
-{
-
-    long p = (argc>1?atoi(argv[1]):5);
-    long e = (argc>2?atoi(argv[2]):3);
-    GFqDom<long> GFq(p, e);
-    GFqDom<long> PrimeField(p,1);
-    std::cout << "Working in GF(" << p << '^' << e << ')' << std::endl;
-    std::cout << "Elements are polynomials in X modulo " << p << std::endl;
-
-    Poly1Dom< GFqDom<long>, Dense > Pdom( PrimeField, Indeter("X") );
-
-    // First get the irreducible polynomial irred
-    // via irred = X^e - mQ
-    // and X^e = X^(e-1) * X
-    GFqDom<long>::Element temo, t, tmp;
-    Poly1Dom< GFqDom<long>, Dense >::Element G, H, J, mQ, irred, modP;
-
-    Pdom.init(G, Degree(GFq.exponent()-1)); // X^(e-1)
-    GFq.init(temo,G); // internal representation
-
-    Pdom.init(H, Degree(1) ); // X
-    GFq.init(t,H); // internal representation
-
-    GFq.init(tmp);
-
-    GFq.mul(tmp, temo, t); // internal representation of X^e
-    GFq.negin(tmp);        // internal representation of -X^e, i.e. of the irreducible polynomial without the largest monomial, X^e
-
-    long lowerpart;
-    // p-adic value of the lower part of the irreducible polynomial
-    GFq.convert(lowerpart, tmp);
-    std::cout << ' ' << p << "-adic value of the lower part of the irreducible : " << lowerpart << std::endl;
-
-    long ptoe = power(p,e); // p-adic value of X^e
-    std::cout << ' ' << p << '^' << e << " is : " << ptoe << std::endl;
-
-    std::cout << " --> Computed irreducible: " << ptoe+lowerpart << std::endl;
-    std::cout << "Stored        irreducible: " << GFq.irreducible() << std::endl;
-
-    Poly1PadicDom< GFqDom<long>, Dense > PAD(Pdom);
-    Poly1PadicDom< GFqDom<long>, Dense >::Element Polynomial;
-
-    PAD.radix(Polynomial, GFq.irreducible());
-
-    std::cout << "Irreducible polynomial coefficients: ";
-    for(Poly1PadicDom< GFqDom<long>, Dense >::Element::iterator it = Polynomial.begin(); it != Polynomial.end(); ++it)
-        PrimeField.write(std::cout << ' ', *it);
-    std::cout << std::endl;
-
-
-    PAD.write(std::cout << "The latter " << GFq.irreducible() << " represents: ", Polynomial)
-            << " in " << p << "-adic"
-            << std::endl;
-
-
-
+int main(int argc, char **argv) {
+    uint64_t plaintext_val[2] = {0x2b58bffc83cc6c39, 0x24f22580b2107da7};
+    uint64_t key_val[14] = {0x27aef6116c4db0e6, 0x2779d02d3094d1df, 0xb8c0ad914767ba80, 0x6ca98308d45d1f79,
+                            0xd75f78588ceaf21a, 0x3190bc4bfa457450, 0x92fd07e27f65d6c2, 0xd632a79fd631870c,
+                            0x235548ef50bd1c1f, 0x002440be99b4d4ba, 0x1d038d1d35d9cd0f, 0xb1336f128aaebf73,
+                            0x8028a087933b6f4a, 0x74fd2d5530ebb1f5};
+    bitset<64> t[2];
+    bitset<N> input, key, rand[6];
+    t[0] = plaintext_val[0];
+    t[1] = plaintext_val[1];
+    for (int i = 0; i < 128; i++)
+        input[i] = i < 64 ? t[0][i] : t[1][i - 64];
+    t[0] = key_val[0];
+    t[1] = key_val[1];
+    for (int i = 0; i < 128; i++)
+        key[i] = i < 64 ? t[0][i] : t[1][i - 64];
+    for (int i = 0; i < 6; i++) {
+        t[0] = key_val[2 * i + 2];
+        t[1] = key_val[2 * i + 3];
+        for (int j = 0; j < 128; j++)
+            rand[i][j] = j < 64 ? t[0][j] : t[1][j - 64];
+    }
+    cout << input << endl << key << endl;
+    for (int i = 0; i < 6; i++)
+        cout << rand[i] << endl;
     return 0;
-
 }
+
