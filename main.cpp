bd9368f75dc0220092793363c017e6d0eafa3060
mairon <mairon.mahzoun@gmail.com>
2022-02-25 17:11:18 +0100

    add 3rd round attack for kcipher

--- main.cpp
+++ main.cpp
@@ -12,7 +12,8 @@ bitset<N> r[6];
 bitset<K> key;
 bitset<N> round_keys[3];
 uint64_t key_table[1 << M][1 << M];
-bitset<N> P[1<<17];
+bitset<N> P[1<<16];
+bitset<N> C[1<<16];
 struct characteristic {
     uint32_t input_diff;
     uint32_t output_diff;
@@ -40,15 +41,17 @@ void Init() {
     bitset<N> rand2[6];
     rand2[0] = 0x4d82b5;
     rand2[1] = 0xdb8760;
-    rand2[2] = 0x02011e;
+    rand2[2] = 0xffffff;
     rand2[3] = 0x71fc2a;
-    rand2[4] = 0xffffff;
+    rand2[4] = 0x4dfa7d;
     rand2[5] = 0xa92a9a;
     for (int i = 0; i < 6; i++)
         r[i] = rand2[i];
-    for (uint32_t i = 0; i < (1<<17); i++)
-        Random(P[i]);
     kcipher.init(key);
+    for (uint32_t i = 0; i < (1<<16); i++) {
+        Random(P[i]);
+        C[i] = kcipher.EncCPA(P[i], key, r);
+    }
 }
 
 inline uint8_t partial_dec(bitset<N> ct, uint8_t r1, uint8_t k, int position, int round) {
@@ -57,7 +60,7 @@ inline uint8_t partial_dec(bitset<N> ct, uint8_t r1, uint8_t k, int position, in
         temp[i] = ct[N - 8 * position + i];
     }
     uint8_t block_val = temp.to_ulong();
-    if(round == 2){
+    if(round <= 2){
         //(s[x] + r) >> 2 + k
         // s[x] >> 2 + r >> 2  + k
         block_val ^= r1;
@@ -86,17 +89,35 @@ inline void differential_cryptanalysis_key_recovery(characteristic c, int idx, i
     p[0] = P[idx];
     p[1] = p[0];
     p[1][c.input_diff] = p[1][c.input_diff] ^ 1;
-    for (int i = 0; i < 2; i++) {
-        ciphertext[i] = kcipher.EncCPA(p[i], key, r);
-        if(round == 2){
-            // veil = k[3] = bitreordering(k[2], 3);
-            bitset<N> veil = kcipher.BitReordering(kcipher.round_keys[2], 3);
-            ciphertext[i] = ciphertext[i] ^ veil;
-            ciphertext[i] = kcipher.Inv_SBox(ciphertext[i], r, 2);
+    if(round > 1) {
+        ciphertext[0] = C[idx];
+        ciphertext[1] = kcipher.EncCPA(p[1], key, r);
+        for (int i = 0; i < 2; i++) {
+            if (round == 2) {
+                // veil = k[3] = bitreordering(k[2], 3);
+                bitset<N> veil = kcipher.BitReordering(kcipher.round_keys[2], 3);
+                ciphertext[i] = ciphertext[i] ^ veil;
+                ciphertext[i] = kcipher.Inv_SBox(ciphertext[i], r, 2);
+                // now we have ciphertext after bitreordering 2
+                ciphertext[i] = ciphertext[i] ^ r[4];
+                ciphertext[i] = kcipher.BitReorderingRev(ciphertext[i], 2);
+                // These amount of decryption is allowed because we already recovered K[3], K[2], and r[5] = r12
+            }
+        }
+    }
+    else if (round == 1){
+        for(int i = 0; i < 2; i++){
+            ciphertext[i] = p[i] + kcipher.round_keys[0];
+            ciphertext[i] = kcipher.BitReordering(ciphertext[i], 0);
+            ciphertext[i] = kcipher.SBox(ciphertext[i], r, 0);
+            ciphertext[i] = ciphertext[i] + kcipher.round_keys[1];
+            ciphertext[i] = kcipher.BitReordering(ciphertext[i], 1);
+            ciphertext[i] = kcipher.SBox(ciphertext[i], r, 1);
+            ciphertext[i] = kcipher.Inv_SBox(ciphertext[i], r, 1);
             // now we have ciphertext after bitreordering 2
-            ciphertext[i] = ciphertext[i] ^ r[4];
-            ciphertext[i] = kcipher.BitReorderingRev(ciphertext[i], 2);
-            // These amount of decryption is allowed because we already recovered K[3], K[2], and r[5] = r12
+            ciphertext[i] = ciphertext[i] ^ r[2];
+            ciphertext[i] = kcipher.BitReorderingRev(ciphertext[i], 1);
+
         }
     }
     uint8_t res[2];
@@ -130,28 +151,28 @@ void differential_cryptanalysis_distinguisher(characteristic c, int round) {
         p1 = P[i];
         p2 = p1;
         p2[c.input_diff] = p2[c.input_diff] ^ 1;
-        c1 = p1 + round_keys[0];
-        c2 = p2 + round_keys[0];
-
+        c1 = p1 + kcipher.round_keys[0];
+        c2 = p2 + kcipher.round_keys[0];
         c1 = kcipher.BitReordering(c1, 0);
         c2 = kcipher.BitReordering(c2, 0);
+        if(round >= 2) {
+            c1 = kcipher.SBox(c1, r, 0);
+            c2 = kcipher.SBox(c2, r, 0);
 
-        c1 = kcipher.SBox(c1, r, 0);
-        c2 = kcipher.SBox(c2, r, 0);
+            //round 2
+            c1 = c1 + kcipher.round_keys[1];
+            c2 = c2 + kcipher.round_keys[1];
 
-        //round 2
-        c1 = c1 + round_keys[1];
-        c2 = c2 + round_keys[1];
-
-        c1 = kcipher.BitReordering(c1, 1);
-        c2 = kcipher.BitReordering(c2, 1);
-        if(round == 3) {
-            c1 = kcipher.SBox(c1, r, 1);
-            c2 = kcipher.SBox(c2, r, 1);
-            c1 = c1 + round_keys[2];
-            c2 = c2 + round_keys[2];
-            c1 = kcipher.BitReordering(c1, 2);
-            c2 = kcipher.BitReordering(c2, 2);
+            c1 = kcipher.BitReordering(c1, 1);
+            c2 = kcipher.BitReordering(c2, 1);
+            if (round == 3) {
+                c1 = kcipher.SBox(c1, r, 1);
+                c2 = kcipher.SBox(c2, r, 1);
+                c1 = c1 + kcipher.round_keys[2];
+                c2 = c2 + kcipher.round_keys[2];
+                c1 = kcipher.BitReordering(c1, 2);
+                c2 = kcipher.BitReordering(c2, 2);
+            }
         }
         if ((c1 ^ c2) == expected_diff) {
             br++;
@@ -165,11 +186,11 @@ void differential_cryptanalysis_distinguisher(characteristic c, int round) {
 }
 
 void attack_round_3() {
-    uint8_t c_arr_1[5][3] = {{22,  16, 1},
+    uint8_t c_arr_1[3][3] = {{22,  16, 1},
                              {15,  13, 2},
                              {19, 2, 3}};
 
-    for (int t = 0; t < 5; t++) {
+    for (int t = 0; t < 3; t++) {
         for (int i = 0; i < 256; i++)
             for (int j = 0; j < 256; j++)
                 key_table[i][j] = 0;
@@ -206,10 +227,10 @@ void attack_round_3() {
 }
 
 void attack_round_2() {
-    uint8_t c_arr_1[5][3] = {{22,  11, 2},
+    uint8_t c_arr_1[3][3] = {{22,  11, 2},
                              {15,  18, 1},
                              {19, 0, 3}};
-    for (int t = 0; t < 5; t++) {
+    for (int t = 0; t < 3; t++) {
         for (int i = 0; i < 256; i++)
             for (int j = 0; j < 256; j++)
                 key_table[i][j] = 0;
@@ -245,6 +266,46 @@ void attack_round_2() {
     }
 }
 
+void attack_round_1() {
+    uint8_t c_arr_1[3][3] = {{22,  17, 1},
+                             {18,  5, 3},
+                             {15, 8, 2}};
+    for (int t = 0; t < 3; t++) {
+        for (int i = 0; i < 256; i++)
+            for (int j = 0; j < 256; j++)
+                key_table[i][j] = 0;
+        characteristic c;
+        c.input_diff = c_arr_1[t][0];
+        c.output_diff = c_arr_1[t][1];
+        c.sbox = c_arr_1[t][2];
+        /*
+         * uncomment the following function call to run the distinguisher attack.
+         */
+        differential_cryptanalysis_distinguisher(c, 1);
+        for (int i = 0; i < (1 << 16); i++) {
+            differential_cryptanalysis_key_recovery(c, i, 1);
+        }
+        int maxk = 0, maxr1 = 0;
+        for (int i = 0; i < 256; i++) {
+            for (int j = 0; j < 256; j++) {
+                if (key_table[i][j] > key_table[maxk][maxr1]) {
+                    maxk = i;
+                    maxr1 = j;
+                }
+            }
+        }
+        cout << c.input_diff << "\t" << c.output_diff << "\t" << c.probability << "\t" << c.sbox << endl;
+        for (int i = 0; i < 256; i++) {
+            for (int j = 0; j < 256; j++) {
+                if (key_table[i][j] == key_table[maxk][maxr1]) {
+                    cout << hex << i << "\t" << j << "\t" << key_table[i][j] << endl;
+                }
+            }
+        }
+        cout << "\n________________________\n";
+    }
+}
+
 using namespace std;
 using chrono::high_resolution_clock;
 using chrono::duration_cast;
@@ -255,14 +316,17 @@ int main(int argc, char **argv) {
     Init();
     ios_base::sync_with_stdio(false);
     auto t1 = high_resolution_clock::now();
-    attack_round_3();
+    attack_round_1();
     auto t2 = high_resolution_clock::now();
-    duration<double, std::milli> ms_double = t2 - t1;
-    cout << "The attack round 3 finished in(ms):" << ms_double.count() << endl;
-    t1 = high_resolution_clock::now();
-    attack_round_2();
-    t2 = high_resolution_clock::now();
-    ms_double = t2 - t1;
-    cout << "The attack round 2 finished in(ms):" << ms_double.count() << endl;
+//    auto t1 = high_resolution_clock::now();
+//    attack_round_3();
+//    auto t2 = high_resolution_clock::now();
+//    duration<double, std::milli> ms_double = t2 - t1;
+//    cout << "The attack round 3 finished in(ms):" << ms_double.count() << endl;
+//    t1 = high_resolution_clock::now();
+//    attack_round_2();
+//    t2 = high_resolution_clock::now();
+//    ms_double = t2 - t1;
+//    cout << "The attack round 2 finished in(ms):" << ms_double.count() << endl;
     return 0;
 }
